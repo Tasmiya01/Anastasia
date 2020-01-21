7a6b02de3954a4c1d7a6497031cf745d886422f9
Tasmiya01 <tasmiyasanaa01@gmail.com>
2020-01-21 19:52:32 +0530

    Final Commit

---
+++ app/src/main/java/com/example/anastasia/activity2.java
@@ -0,0 +1,14 @@
+package com.example.anastasia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activity2);
+    }
+}
